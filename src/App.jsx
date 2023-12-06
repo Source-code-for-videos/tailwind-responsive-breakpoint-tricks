81f7cf1be7838d17528a1b0d7e40974061c222fe
Asfak Ahmed <asfakahmed680@gmail.com>
2023-12-06 12:23:58 +0600

    done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 w-full h-full place-items-center gap-6">
+      <div className="card1 "></div>
+      <div className="card1 "></div>
+      <div className="card1 "></div>
+      <div className="card1 "></div>
+      <div className="card1 "></div>
+      <div className="card1 "></div>
+      <div className="card1 "></div>
+      <div className="card1 "></div>
+    </div>
+  );
+};
+
+export default App;
